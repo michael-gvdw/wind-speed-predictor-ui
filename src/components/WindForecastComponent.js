3a0b03e10a6464c56f7cfd6464e1efbb4be228d1
Michael Groenewegen van der Weijden <michael@fragrant-chios.com>
2021-04-15 19:56:26 +0200

    added methods and interactivity

--- src/components/WindForecastComponent.js
+++ src/components/WindForecastComponent.js
@@ -4,25 +4,28 @@ import React, { useEffect, useState } from 'react';
 import WindForecastPage from '../pages/WindForecastPage'
 
 const WindForecastComponent = () => {
+    const d = new Date();
 
-    const [date, set_date] = useState(null)
-    const [wind_speed, set_wind_speed] = useState(5) 
+    const [day, set_day] = useState(d.getDate())
+    const [month, set_month] = useState(d.getMonth() + 1)
+    const [year, set_year] = useState(d.getFullYear())
+
+    const [wind_speed, set_wind_speed] = useState(5.6) 
+    const [wind_speed_lower, set_wind_speed_lower] = useState(2.4)
+    const [wind_speed_upper, set_wind_speed_upper] = useState(7.1)
+
     const [wind_speed_multiplier, set_wind_speed_multiplier] = useState(0)
     const [wind_speed_multipliers, set_wind_speed_multipliers] = useState(
                                                                         [
-                                                                            {id: 0, scale: 'm/s'}, 
-                                                                            {id: 1, scale: 'knots'}, 
-                                                                            {id: 2, scale: 'beaufort'},
+                                                                            {id: 0, scale: 'm/s', value: 1}, 
+                                                                            {id: 1, scale: 'knots', value: 1.94384}, 
+                                                                            {id: 2, scale: 'beaufort', value: 1.1268406556253},
                                                                         ])
 
     useEffect(() => {
-
+        
     }, [])
 
-    const handle_date_change = (event) => {
-        const { name, value } = event.target
-        set_date(value)
-    }
 
     const handle_wind_speed_multiplier_change = (event) => {
         const { name, value } = event.target
@@ -33,16 +36,36 @@ const WindForecastComponent = () => {
         else {
             set_wind_speed_multiplier(value)
         }    
+    	
+    }
+
+    const fetch_wind_speed = (event) => {
+        event.preventDefault()
+        
+        const date = `${day}-${month}-${year}`
+        console.log(`click: ${date}`)
     }
 
     return (
         <WindForecastPage 
-            handle_date_change={handle_date_change} 
             handle_wind_speed_multiplier_change={handle_wind_speed_multiplier_change}
+            
             wind_speed_multiplier={wind_speed_multiplier}
             wind_speed_multipliers={wind_speed_multipliers}
-            date={date}
+
+            day={day}
+            month={month}
+            year={year}
+
+            set_day={set_day}
+            set_month={set_month}
+            set_year={set_year}
+
             wind_speed={wind_speed}
+            wind_speed_lower={wind_speed_lower}
+            wind_speed_upper={wind_speed_upper}
+
+            fetch_wind_speed={fetch_wind_speed}
         />
     )
 }
--- src/pages/WindForecastPage.js
+++ src/pages/WindForecastPage.js
@@ -3,32 +3,36 @@ import React from 'react'
 import '../styles/wind.scss'
 
 
-const WindForecastPage = ({ handle_date_change, handle_wind_speed_multiplier_change, 
-                            wind_speed_multiplier, wind_speed_multipliers, date, wind_speed }) => {
+const WindForecastPage = ({ handle_wind_speed_multiplier_change, 
+                            wind_speed_multiplier, wind_speed_multipliers,
+                            day, month, year, 
+                            set_day, set_month, set_year, 
+                            wind_speed, wind_speed_lower, wind_speed_upper, 
+                            fetch_wind_speed }) => {
     return (
         <div className={`wind-forecast`}>
 
             <div className={`header`}>
                 <span>wind speed in: </span>
 
-                <select value={wind_speed_multiplier} onChange={handle_wind_speed_multiplier_change}>
+                <select value={wind_speed_multiplier} onChange={handle_wind_speed_multiplier_change} disabled={true}>
                     {wind_speed_multipliers.map(multiplier => <option key={multiplier.id} value={multiplier.id}>{multiplier.scale}</option>)}
                 </select>
             </div>
 
             <div className={`wind-values`}>
-                <WindValue key={0} label={`prediction:`} value={5.6} />
-                <WindValue key={1} label={`lower limit:`} value={2.4} />
-                <WindValue key={2} label={`upper limit:`} value={7.1} />
+                <WindValue key={0} label={`prediction:`} value={wind_speed} />
+                <WindValue key={1} label={`lower limit:`} value={wind_speed_lower} />
+                <WindValue key={2} label={`upper limit:`} value={wind_speed_upper} />
             </div>
 
             <form>
                 <div className={`select-group`}>
-                    <FormSelect start={1} end={32} label={`day:`} />
-                    <FormSelect start={1} end={13} label={`month:`} />
-                    <FormSelect start={2021} end={2022} label={`year:`}/>
+                    <FormSelect start={1} end={32} label={`day:`} value={day} handle_change={set_day} />
+                    <FormSelect start={1} end={13} label={`month:`} value={month} handle_change={set_month} />
+                    <FormSelect start={2021} end={2022} label={`year:`} value={year} handle_change={set_year} />
                 </div>
-                <input type={`submit`} value={`submit`} />
+                <input type={`submit`} value={`submit`} onClick={fetch_wind_speed}/>
             </form> 
 
         </div>
@@ -48,15 +52,19 @@ const FormSelect = (props) => {
 
     let items = []
     for (let i=props.start; i<props.end; i++) {
-        items.push(<option key={i}>{ i }</option>)
+        items.push(<option key={i} value={i}>{ i }</option>)
     }
 
     return (
         <div className={`select-item`}>
             <label>{ props.label }</label>
-            <select>{ items }</select>
+
+            <select 
+                value={props.value} 
+                onChange={(event) => props.handle_change(event.target.value)} 
+            >{ items }</select>
         </div>
-    )
+    )  
 }
 
 export default WindForecastPage
